8ffae2086fe6237ada47bf710cb22b671774e723
abtahi-tajwar <abtahitajwar@gmail.com>
2021-02-01 12:56:33 +0600

    Classwork 02

---
+++ classwork02/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classwork 02</title>
+</head>
+<body>
+    <h1>BUTTER CHICKEN</h1>
+    <p>Butter chicken originated from Northern India in 1948. Created by Kundan Lal Gujral, you may notice that Butter Chicken (known as murgh makhani — chicken with butter), is similar to British tikka masala.
+
+    Prepared in a buttery gravy with the addition of cream gives the curry sauce a silky smooth rich texture. Most restaurants are known to add in copious amounts of butter, which as a result can leave you feeling bloated or ill from the slick of grease.
+
+    However, we found while recipe testing that we prefer to start the sauce off in Ghee (clarified butter, or you can use a combination of half regular butter and half oil). .....</p>
+    <img src="img/one.jpeg" alt="Food Image" height="250"> <br />
+    <a href="http://chewitup.net/cooking/how-to-make-butter-chicken/">Read The Full Article Here</a><br /><br />
+
+    <h2>Subscribe to our newsletter</h2>
+
+    <form action="/">
+        <table>
+            <tr>
+                <td><label for="fname">First Name: </label></td>
+                <td><input name="fname" id="fname" type="text" /><br /></td>
+            </tr>
+            <tr>
+                <td><label for="lname">Last Name: </label></td>
+                <td><input name="lname" id="lname" type="text" /><br /></td>
+            </tr>
+            <tr>
+                <td><label for="email">Email: </label></td>
+                <td><input name="text" id="email" type="email" /><br /></td>
+            </tr>
+            <tr>
+                <td><label for="password">Password: </label></td>
+                <td><input name="password" id="password" type="password" /><br /></td>
+            </tr>
+            <tr>
+                <td><label for="confirm_password">Confirm Password: </label></td>
+                <td><input name="confirm_password" id="confirm_password" type="password" /><br /></td>
+            </tr>
+            <tr>
+                <td><label for="dob">Birth Date: </label></td>
+                <td><input name="dob" id="dob" type="date" /><br /></td>
+            </tr>
+            <tr>
+                <td><input type="radio" id="male" name="gender" value="male">
+                <label for="male">Male</label></td>
+                <td><input type="radio" id="female" name="gender" value="female">
+                <label for="female">Female</label></td>
+                <td><input type="radio" id="other_gender" name="gender" value="other_gender">
+                <label for="other_gender">Other</label></td>
+            </tr>
+            <tr>
+            <td><label for="color">Choose your preferred color</label></td>
+            <td><input type="color"></td>
+            </tr>
+            <tr>
+            <td><input type="submit" value="submit">
+            <input type="button" value="reset" /></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ classwork02/page2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classwork 02</title>
+</head>
+<body>
+    <h1>BUTTER CHICKEN</h1>
+    <p>Butter chicken originated from Northern India in 1948. Created by Kundan Lal Gujral, you may notice that Butter Chicken (known as murgh makhani — chicken with butter), is similar to British tikka masala.
+
+    Prepared in a buttery gravy with the addition of cream gives the curry sauce a silky smooth rich texture. Most restaurants are known to add in copious amounts of butter, which as a result can leave you feeling bloated or ill from the slick of grease.
+
+    However, we found while recipe testing that we prefer to start the sauce off in Ghee (clarified butter, or you can use a combination of half regular butter and half oil). .....</p>
+    <img src="img/one.jpeg" alt="Food Image" height="250"> <br />
+    <a href="http://chewitup.net/cooking/how-to-make-butter-chicken/">Read The Full Article Here</a><br /><br />
+
+
+    <form action="/">
+        <label for="fname">First Name: </label>
+        <input name="fname" id="fname" type="text"><br />
+        <label for="lname">Last Name: </label>
+        <input name="lname" id="lname" type="text"><br />
+        <label for="email">Email: </label>
+        <input name="email" id="email" type="email"><br />
+        <label for="password">Password: </label>
+        <input name="password" id="password" type="password"><br />
+        <label for="confirm_password">Confirm Password: </label>
+        <input name="confirm_password" id="confirm_password" type="password"><br />
+        <label for="dob">Birth Date: </label>
+        <input name="dob" id="dob" type="date"><br />
+
+    </form>
+</body>
+</html>
